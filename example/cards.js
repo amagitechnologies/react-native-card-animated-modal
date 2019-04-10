163525dbaa6268c1f34abbc96917482f57ed71dd
Archie Herbias <archie.herbias@gmail.com>
2019-04-10 22:07:54 +0800

    Updated examples with new props

--- example/cards.js
+++ example/cards.js
@@ -1,9 +1,11 @@
 /* eslint react/prop-types : off */
+/* eslint no-unused-vars : off */
 import React from "react";
 
 import { View, Text, TouchableOpacity, Image, Dimensions } from "react-native";
 
 import IGIcon from "./assets/ig_icon";
+import BattleFieldIcon from "./assets/battlefield_icon";
 import CreativeImage from "./assets/creative";
 import IGImage from "./assets/ig_image";
 import FitnessImage from "./assets/fitness_image";
@@ -13,18 +15,12 @@ const { height } = Dimensions.get("window");
 
 export default [
   {
-    source: {
+    image: {
       uri: CreativeImage
     },
+    height: height * 0.7,
     renderItem: ({ item }) => (
       <View>
-        <Image
-          source={item.source}
-          style={{
-            width: "100%",
-            height: height * 0.5
-          }}
-        />
         <View
           style={{
             width: "100%",
@@ -57,18 +53,11 @@ export default [
     )
   },
   {
-    source: {
+    image: {
       uri: IGImage
     },
     renderItem: ({ item }) => (
       <View>
-        <Image
-          source={item.source}
-          style={{
-            width: "100%",
-            height: height * 0.6
-          }}
-        />
         <View
           style={{
             width: "100%",
@@ -127,18 +116,11 @@ export default [
     )
   },
   {
-    source: {
+    image: {
       uri: FitnessImage
     },
     renderItem: ({ item }) => (
       <View>
-        <Image
-          source={item.source}
-          style={{
-            width: "100%",
-            height: height * 0.6
-          }}
-        />
         <View
           style={{
             width: "100%",
@@ -174,19 +156,27 @@ export default [
     )
   },
   {
-    source: {
+    image: {
       uri: GameImage
     },
+    height: height * 0.8,
     renderItem: ({ item }) => (
       <View
         style={{
-          backgroundColor: "rgba(255, 255, 255, 0.7)"
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%"
         }}
       >
         <View
           style={{
+            position: "absolute",
+            top: 0,
             width: "100%",
-            padding: 16
+            padding: 16,
+            backgroundColor: "rgba(255, 255, 255, 0.7)"
           }}
         >
           <Text
@@ -210,13 +200,54 @@ export default [
             Get your nerves in the game
           </Text>
         </View>
-        <Image
-          source={item.source}
+        <View
           style={{
+            position: "absolute",
+            bottom: 0,
             width: "100%",
-            height: height * 0.4
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+            backgroundColor: "rgb(255, 255, 255)",
+            padding: 10
           }}
-        />
+        >
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center"
+            }}
+          >
+            <Image
+              source={{ uri: BattleFieldIcon }}
+              style={{ width: 40, height: 40, marginRight: 10 }}
+            />
+            <View>
+              <Text style={{ fontSize: 12, fontWeight: "bold" }}>
+                Battlefield
+              </Text>
+              <Text style={{ fontSize: 12 }}>Get on the battle field!</Text>
+            </View>
+          </View>
+          <View>
+            <TouchableOpacity
+              style={{
+                backgroundColor: "#007AFF",
+                padding: 7,
+                minWidth: 60,
+                borderRadius: 16
+              }}
+            >
+              <Text
+                style={{ color: "rgb(255, 255, 255)", textAlign: "center" }}
+              >
+                GET
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     )
   }
